36a67c8333fc14d69fe9a0ab6a07237cde4362c9
caothaito <54255705+caothaito@users.noreply.github.com>
2020-08-16 21:56:03 +0700

    a

--- bogo/assets/js/main.js
+++ bogo/assets/js/main.js
@@ -1,23 +1,45 @@
-$(document).ready(function(){
-    $("#topLogo").click(function(){
-        $(".plant-trees-account").slideToggle();
-    })
+$(document).ready(function () {
+  $("#topLogo").click(function () {
+    $(".plant-trees-account").slideToggle();
+  })
 })
 
-(function() {
+// (function () {
 
-    var hamburger = {
-      navToggle: document.querySelector('.nav-toggle'),
-      nav: document.querySelector('nav'),
-  
-      doToggle: function(e) {
-        e.preventDefault();
-        this.navToggle.classList.toggle('expanded');
-        this.nav.classList.toggle('expanded');
-      }
-    };
-  
-    hamburger.navToggle.addEventListener('click', function(e) { hamburger.doToggle(e); });
-    hamburger.nav.addEventListener('click', function(e) { hamburger.doToggle(e); });
-  
-  }());
+//   var hamburger = {
+//     navToggle: document.querySelector('.nav-toggle'),
+//     nav: document.querySelector('nav'),
+
+//     doToggle: function (e) {
+//       e.preventDefault();
+//       this.navToggle.classList.toggle('expanded');
+//       this.nav.classList.toggle('expanded');
+//     }
+//   };
+
+//   hamburger.navToggle.addEventListener('click', function (e) {
+//     hamburger.doToggle(e);
+//   });
+//   hamburger.nav.addEventListener('click', function (e) {
+//     hamburger.doToggle(e);
+//   });
+
+// }());
+
+
+// =====FAQs=========
+const items = document.querySelectorAll(".accordion button");
+
+function toggleAccordion() {
+  const itemToggle = this.getAttribute('aria-expanded');
+
+  for (i = 0; i < items.length; i++) {
+    items[i].setAttribute('aria-expanded', 'false');
+  }
+
+  if (itemToggle == 'false') {
+    this.setAttribute('aria-expanded', 'true');
+  }
+}
+
+items.forEach(item => item.addEventListener('click', toggleAccordion));
